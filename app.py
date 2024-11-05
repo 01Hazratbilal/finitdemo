08fc1573796cbc05a44735b094b718891c449e03
Hazrat Bilal <101354929+01Hazratbilal@users.noreply.github.com>
2024-11-05 23:18:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-from google.oauth2 import service_account
-from google.analytics.data import BetaAnalyticsDataClient
-from google.analytics.data_v1beta.types import RunRealtimeReportRequest, Dimension, Metric
-from datetime import datetime, timedelta
-
-# Google Analytics setup
-PROPERTY_ID = "465906322"
-
 import json
-st.write(st.secrets)
+from google.oauth2 import service_account
+from google.cloud import documentai  # or any other Google Cloud library you need
+
+# Load the credentials from Streamlit secrets
+credentials_info = json.loads(st.secrets["gcs_connections"]["GOOGLE_APPLICATION_CREDENTIALS"])
+
+# Create credentials object
+credentials = service_account.Credentials.from_service_account_info(credentials_info)
+
+# Initialize your client (for example, Document AI client)
+client = documentai.DocumentProcessorServiceClient(credentials=credentials)
+
+# Now you can use the client to interact with Google Cloud services
