5c057299ecab7df16655fc81f82103c41b45a427
Hazrat Bilal <101354929+01Hazratbilal@users.noreply.github.com>
2024-11-05 16:48:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
-import streamlit as st
-from google.oauth2 import service_account
+import os
 import json
+import streamlit
 
-# Load Google credentials from Streamlit secrets
-try:
-    json_credentials = st.secrets["general"]["GOOGLE_CREDENTIALS"]
-    credentials_dict = json.loads(json_credentials)
-    credentials = service_account.Credentials.from_service_account_info(credentials_dict)
-except KeyError:
-    st.error("Google credentials are not set in the environment variables.")
-    st.stop()
-except json.JSONDecodeError:
-    st.error("Failed to decode JSON from GOOGLE_CREDENTIALS.")
-    st.stop()
-except Exception as e:
-    st.error(f"An error occurred: {e}")
-    st.stop()
-print(st.secrets)
+google_credentials = st.secrets("GOOGLE_CREDENTIALS")
+st.write(google_credentials)  # Log the credentials
+credentials = json.loads(google_credentials)
