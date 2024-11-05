f5ef0e6876cc373ad987beb6e8a42f86319481bc
Hazrat Bilal <101354929+01Hazratbilal@users.noreply.github.com>
2024-11-05 15:37:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from google.oauth2 import service_account
+import json
 
 # Load Google credentials from Streamlit secrets
 try:
     json_credentials = st.secrets["GOOGLE_CREDENTIALS"]
-    credentials = service_account.Credentials.from_service_account_info(json_credentials)
+    credentials = service_account.Credentials.from_service_account_info(json.loads(json_credentials))
 except KeyError:
     st.error("Google credentials are not set in the environment variables.")
     st.stop()  # Stop the execution if the secret is not found
-
-# Initialize the client
-client = BetaAnalyticsDataClient(credentials=credentials)
+except ValueError as e:
+    st.error(f"Failed to parse Google credentials: {e}")
+    st.stop()
