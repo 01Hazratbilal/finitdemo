9c5e556ce4d9fea19c032f222d934050f52f423a
Hazrat Bilal <101354929+01Hazratbilal@users.noreply.github.com>
2024-11-05 23:29:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import json
 
+st.write(st.secrets)
+
 # Check if 'gcs_connections' exists in st.secrets
 if "gcs_connections" in st.secrets:
     credentials_info = json.loads(st.secrets["gcs_connections"]["GOOGLE_APPLICATION_CREDENTIALS"])
